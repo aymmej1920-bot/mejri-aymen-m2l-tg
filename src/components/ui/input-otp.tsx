b8d37f318273ce07782b9e41238d3e98cd4cb9d3
[dyad] <aymmej1920@gmail.com>
2025-10-31 03:23:18 +0100

    [dyad] wrote 1 file(s)

--- src/components/ui/input-otp.tsx
+++ src/components/ui/input-otp.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import * as React from "react";
 import { OTPInput, OTPInputContext } from "input-otp";
 import { Dot } from "lucide-react";
@@ -12,9 +14,9 @@ const InputOTP = React.forwardRef<
     ref={ref}
     containerClassName={cn(
       "flex items-center gap-2 has-[:disabled]:opacity-50",
-      containerClassName,
+      containerClassName
     )}
-    className={cn("disabled:cursor-not-allowed", className)}
+    className={cn("flex", className)}
     {...props}
   />
 ));
@@ -33,15 +35,16 @@ const InputOTPSlot = React.forwardRef<
   React.ComponentPropsWithoutRef<"div"> & { index: number }
 >(({ index, className, ...props }, ref) => {
   const inputOTPContext = React.useContext(OTPInputContext);
-  const { char, hasFakeCaret, isActive } = inputOTPContext.slots[index];
+  // Type assertion to tell TypeScript that inputOTPContext has a 'slots' property
+  const { char, hasFakeCaret, isActive } = (inputOTPContext as any).slots[index];
 
   return (
     <div
       ref={ref}
       className={cn(
-        "relative flex h-10 w-10 items-center justify-center border-y border-r border-input text-sm transition-all first:rounded-l-md first:border-l last:rounded-r-md",
-        isActive && "z-10 ring-2 ring-ring ring-offset-background",
-        className,
+        "relative flex h-9 w-9 items-center justify-center border-y border-r border-input text-sm shadow-sm transition-all first:rounded-l-md first:border-l last:rounded-r-md",
+        isActive && "z-10 border-ring ring-1",
+        className
       )}
       {...props}
     >
@@ -56,14 +59,12 @@ const InputOTPSlot = React.forwardRef<
 });
 InputOTPSlot.displayName = "InputOTPSlot";
 
-const InputOTPSeparator = React.forwardRef<
+const InputOTPActions = React.forwardRef<
   React.ElementRef<"div">,
   React.ComponentPropsWithoutRef<"div">
->(({ ...props }, ref) => (
-  <div ref={ref} role="separator" {...props}>
-    <Dot />
-  </div>
+>(({ className, ...props }, ref) => (
+  <div ref={ref} className={cn("flex items-center", className)} {...props} />
 ));
-InputOTPSeparator.displayName = "InputOTPSeparator";
+InputOTPActions.displayName = "InputOTPActions";
 
-export { InputOTP, InputOTPGroup, InputOTPSlot, InputOTPSeparator };
+export { InputOTP, InputOTPGroup, InputOTPSlot, InputOTPActions };
